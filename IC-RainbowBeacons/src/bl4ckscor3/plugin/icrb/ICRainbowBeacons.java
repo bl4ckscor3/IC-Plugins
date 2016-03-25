0a992631b7223811e32b82b2a4a33348bd558b8c
bl4ckscor3 <donalddani9612@gmx.de>
2016-03-25 18:30:25 +0100

    mavenize | add de-/activation

--- IC-RainbowBeacons/src/bl4ckscor3/plugin/icrb/ICRainbowBeacons.java
+++ IC-RainbowBeacons/src/bl4ckscor3/plugin/icrb/ICRainbowBeacons.java
@@ -1,12 +1,66 @@
 package bl4ckscor3.plugin.icrb;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
 
-public class ICRainbowBeacons extends JavaPlugin
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class ICRainbowBeacons extends JavaPlugin implements Listener
 {
-	@Override
-	public void onEnable(){}
+	private static final HashMap<Location,Integer> activeBeacons = new HashMap<Location,Integer>(); //location of the block, task id
+	private static ICRainbowBeacons instance;
 	
 	@Override
-	public void onDisable(){}
+	public void onEnable()
+	{
+		instance = this;
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	public void onBlockRedstone(BlockRedstoneEvent event)
+	{
+		if(event.getBlock().getBlockPower() > 0)
+		{
+			BeaconRunnable r = new BeaconRunnable(event.getBlock());
+			
+			r.runTaskTimer(this, 0, 5);
+			activeBeacons.put(event.getBlock().getLocation(), r.getTaskId());
+		}
+		else
+			getServer().getScheduler().cancelTask(activeBeacons.remove(event.getBlock().getLocation()));
+	}
+	
+	public class BeaconRunnable extends BukkitRunnable implements BukkitTask
+	{
+		private Block b;
+		
+		public BeaconRunnable(Block block)
+		{
+			b = block;
+		}
+		
+		@Override
+		public void run()
+		{
+			
+		}
+
+		@Override
+		public Plugin getOwner()
+		{
+			return instance;
+		}
+
+		@Override
+		public boolean isSync()
+		{
+			return false;
+		}
+	}
 }

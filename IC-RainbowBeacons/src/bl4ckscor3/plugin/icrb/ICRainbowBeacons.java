08fa3b579987be7c9fed600b631f068457dcefcc
bl4ckscor3 <donalddani9612@gmx.de>
2016-03-26 14:42:44 +0100

    wip stuff

--- IC-RainbowBeacons/src/bl4ckscor3/plugin/icrb/ICRainbowBeacons.java
+++ IC-RainbowBeacons/src/bl4ckscor3/plugin/icrb/ICRainbowBeacons.java
@@ -3,7 +3,9 @@ package bl4ckscor3.plugin.icrb;
 import java.util.HashMap;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
 import org.bukkit.plugin.Plugin;
@@ -15,40 +17,50 @@ public class ICRainbowBeacons extends JavaPlugin implements Listener
 {
 	private static final HashMap<Location,Integer> activeBeacons = new HashMap<Location,Integer>(); //location of the block, task id
 	private static ICRainbowBeacons instance;
-	
+
 	@Override
 	public void onEnable()
 	{
 		instance = this;
 		getServer().getPluginManager().registerEvents(this, this);
 	}
-	
+
+	@EventHandler
 	public void onBlockRedstone(BlockRedstoneEvent event)
 	{
-		if(event.getBlock().getBlockPower() > 0)
+		System.out.println(event.getBlock().getType().toString());
+		
+		if(event.getBlock().getType() == Material.BEACON)
 		{
-			BeaconRunnable r = new BeaconRunnable(event.getBlock());
-			
-			r.runTaskTimer(this, 0, 5);
-			activeBeacons.put(event.getBlock().getLocation(), r.getTaskId());
+			if(event.getBlock().getBlockPower() > 0)
+			{
+				System.out.println("ACTIVE");
+//				BeaconRunnable r = new BeaconRunnable(event.getBlock());
+//
+//				r.runTaskTimer(this, 0, 5);
+//				activeBeacons.put(event.getBlock().getLocation(), r.getTaskId());
+			}
+			else
+			{
+				System.out.println("NOT ACTIVE");
+//				getServer().getScheduler().cancelTask(activeBeacons.remove(event.getBlock().getLocation()));
+			}
 		}
-		else
-			getServer().getScheduler().cancelTask(activeBeacons.remove(event.getBlock().getLocation()));
 	}
-	
+
 	public class BeaconRunnable extends BukkitRunnable implements BukkitTask
 	{
 		private Block b;
-		
+
 		public BeaconRunnable(Block block)
 		{
 			b = block;
 		}
-		
+
 		@Override
 		public void run()
 		{
-			
+
 		}
 
 		@Override
